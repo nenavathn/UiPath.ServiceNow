5e0906f07dd02e43797f3b1d68b4ee934d335cab
Dinesh <dbalasubram7@csc.com>
2018-08-07 12:59:33 +0530

    Add project files.

---
+++ ServiceNow/GetIncident.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
+using System.Security;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json.Linq;
+
+namespace ServiceNow
+{  
+
+    public class GetIncident : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<String> SnowInstance { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<String> UserName { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<String> Password { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<String> IncidentNumber { get; set; }
+
+        [Category("Output")]
+        public OutArgument<Object> IncidentObject { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var userName = UserName.Get(context);
+            var password = Password.Get(context).ToString();
+
+            Uri callUri = new Uri((SnowInstance.Get(context) + "/api/now/table/incident?sysparm_query=number=" + IncidentNumber), UriKind.Absolute);
+
+            var client = new RestClient(callUri);
+            client.Authenticator = new HttpBasicAuthenticator(userName, password);
+
+            var request = new RestRequest(Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            JObject json = JObject.Parse(response.Content);
+
+            Object resp1 = json;
+
+            IncidentObject.Set(context, resp1);
+        }
+
+    }
+}
---
+++ ServiceNow/GetIncidents.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
+using System.Security;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json.Linq;
+using ActivityDesignerLibrary2;
+
+namespace ServiceNow
+{
+    public class GetIncidents : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<String> SnowInstance { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<String> UserName { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<String> Password { get; set; }
+
+        [Category("Output")]
+        public OutArgument<JObject> IncidentList { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var userName = UserName.Get(context);
+            var password = Password.Get(context).ToString();
+
+            Uri callUri = new Uri((SnowInstance.Get(context) + "/api/now/table/incident"), UriKind.Absolute);
+
+            var client = new RestClient(callUri);
+            client.Authenticator = new HttpBasicAuthenticator(userName, password);
+
+            var request = new RestRequest(Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            JObject json = JObject.Parse(response.Content);
+
+            Console.WriteLine("response - " + response.Content);
+
+            Console.WriteLine("json - " + json.ToString());
+
+
+            //Object resp1 = json;
+
+            IncidentList.Set(context, json);
+        }
+    }
+}
